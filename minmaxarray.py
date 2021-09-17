539243211f01078f5d15511a90f49a613073a1bc
Baibhab Chakraborty <baibhab.baishakhi@gmail.com>
2021-09-17 21:53:04 +0530

    min max array

---
+++ minmaxarray.py
@@ -0,0 +1,23 @@
+from array import *
+
+arr = array('i',[])
+
+
+n = int(input("Enter size of array:"))
+
+for i in range(n):
+    x = int(input("Enter array element:"))
+    arr.append(x)
+
+min = max = arr[0]
+
+
+for i in range(n):
+    if(arr[i] > max):
+        max = arr[i]
+    
+    if(arr[i] < min):
+        min = arr[i]
+
+print("Minimum element is ", min)
+print("Maximum element is ", max)
