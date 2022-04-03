66e1f2b50b80aab2ef4a2495706a91639e233686
Baibhab Chakraborty <baishakhi.baibhab@gmail.com>
2022-04-03 11:57:50 +0530

    day 10

---
+++ arrays/stockSpan.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class stockSpan
+{
+    public static void main(String[] args)
+    {
+        Scanner S = new Scanner(System.in);
+        int t = S.nextInt();
+
+        while(t-- > 0)
+        {
+            int n = S.nextInt();
+            
+        }
+    }
+}
